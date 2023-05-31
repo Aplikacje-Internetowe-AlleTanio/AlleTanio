0d8868f0fd1b6e625b93f2e0d0f34fc57fcf381d
sano219 <sano219@outlook.com>
2023-05-31 20:43:18 +0200

    Dodawanie i wyświetlanie ocen produktu

--- src/backend/routes/index.ts
+++ src/backend/routes/index.ts
@@ -1,10 +1,16 @@
 import express from 'express'
 import getStatus from './status/get.status'
-import postUser from './user/post.user'
-import loginUser from './user/login.user'
-import postAddress from './address/post.address'
-import postAddProduct from './product/post.add_product'
-import postOrder from './product/post.order'
+
+import postUser from './user/post.user' //Rejestracja
+import loginUser from './user/login.user' //Logowanie
+
+import postAddress from './address/post.address' //Dodawanie adresów
+
+import postAddProduct from './product/post.add_product' //Dodawanie produktu
+import postOrder from './product/post.order' //Składanie zamówień
+
+import postRate from './rate/post.rate' //Ocenianie produktu
+import getRating from './rate/get.rating' //Wyświetlanie ocen
 
 const router = express.Router()
 // middleware
@@ -17,7 +23,7 @@ router.get('/', (req, res) => {
     res.send('Example home page')
 })
 // api route
-const apiRoutes = [getStatus, postUser, loginUser, postAddress, postAddProduct, postOrder]
+const apiRoutes = [getStatus, postUser, loginUser, postAddress, postAddProduct, postOrder, postRate, getRating]
 
 apiRoutes.forEach((route) =>
     router[route.method](route.path, route.validators, route.handler),
